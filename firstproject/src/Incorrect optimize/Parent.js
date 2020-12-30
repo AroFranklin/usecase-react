b6786cf6615d8ce17c61c965e66a586f66fd9bf8
Aro Franklin <codeblogsite@gmail.com>
2020-12-30 21:42:51 +0530

    localstorage

--- firstproject/src/Incorrect optimize/Parent.js
+++ firstproject/src/Incorrect optimize/Parent.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 function Parent() {
+    
+        localStorage.setItem('ID',121)
+    
     return (
         <div>
-            
+          <h1> hi {localStorage.getItem('ID')}</h1>
         </div>
     )
 }
